0226e897b4bb3e94a75b832cf7ac152605afb2da
Shamel Lakin <christlife4u@gmail.com>
2019-09-21 18:04:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+flatiron = {instructors: 50, students: 1,000, grads: 5,00}
 
 def key_for_min_value(name_hash)
 
