458dfccc662484566e6f20628e1dd9aa1b7b9b6b
Shamel Lakin <christlife4u@gmail.com>
2019-09-21 18:07:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 flatiron = {instructors: 50, students: 1,000, grads: 5,00}
 
-def key_for_min_value(name_hash)
+def key_for_min_value(flatiron)
+  flatiron.collect do |instructors, students, grads|
+  
+  end
 
 end
