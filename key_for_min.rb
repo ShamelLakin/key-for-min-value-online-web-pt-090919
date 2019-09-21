8b209d0414f416b7218da20534fc825f3711287b
Shamel Lakin <christlife4u@gmail.com>
2019-09-21 18:44:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ flatiron = {instructors: 50, students: 1,000, grads: 5,00}
 
 def key_for_min_value(flatiron)
   flatiron.collect do |k, v|
-  puts k if v == hash.values
+  puts k if v > hash.values
   
   end
 
